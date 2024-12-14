2f058f76cc0d9384cb503ac0aaeafcae0512065e
kribien <lucastranslate9@gmail.com>
2024-12-14 09:17:55 +0100

    Add files via upload

---
+++ src/index.php
@@ -0,0 +1,11 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link href="./dist/styles.css" rel="stylesheet">  
+    <title>Document</title>
+</head>
+<body>
+    include("test.php")
+</body>
+</html>
